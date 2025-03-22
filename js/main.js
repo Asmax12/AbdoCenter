e831a0970f1d6971da8424edecb867502b9ac12a
unknown <omargoda547@gmail.com>
2025-03-22 23:30:00 +0200

    Style.css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const toggleBtn = document.querySelector(".toggle-btn");
 const toggleBtnIcon = document.querySelector(".toggle-btn i");
 const dropDownMenu = document.querySelector(".dropdown-menu");
 
-toggleBtn.onclick = function () {
+toggleBtn.ongit onclick = function () {
   dropDownMenu.classList.toggle("open");
   const isOpen = dropDownMenu.classList.contains("open");
 
